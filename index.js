d0d4f748dac79eebb107d705f536bf864f08ec00
jaxon grosam <jaxon.grosam@gmail.com>
2023-08-14 12:10:18 -0500

    added inquirer and cleaned up code

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
-// TODO: Create an array of questions for user input
 const questions = [
   {
     type: "input",
@@ -53,17 +52,14 @@ const questions = [
   },
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((inquirerResponses) => {
     writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
   });
 }
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
@@ -7,17 +5,12 @@ function renderLicenseBadge(license) {
   return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
     return `\n* [License](#license)\n`;
   }
   return "";
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== "None") {
     return `## License
@@ -26,7 +19,6 @@ function renderLicenseSection(license) {
   return "";
 }
 
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
@@ -34,14 +26,15 @@ function generateMarkdown(data) {
   ${data.description}
 
   ## Table of Contents
-  *[Installation](#installation)
-  *[Usage](#usage)
-  ${renderLicenseLink(data.license)}
-  *[Contributors](#contributors)
-  *[Tests](#tests)
-  *[Questions](#questions)
 
-  ## Installation
+  \n* [Installation](#installation)\n
+  \n* [Usage](#usage)\n
+  ${renderLicenseLink(data.license)}
+  \n* [Contributors](#contributors)\n
+  \n* [Tests](#tests)\n
+  \n* [Questions](#questions)\n
+
+  \n## Installation
   ${data.installation}
 
   ## Usage 
